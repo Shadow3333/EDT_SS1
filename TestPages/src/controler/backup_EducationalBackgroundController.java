cc101e43e283b8a16b484e763e90ad99f9fad5a0
ares <ares@git.git>
2016-03-16 16:05:30 +0100

    bugs correction new prob with checkbox list the selected TU must be return as EUs but are return as String proposed solution : use <f:converter/>
    Signed-off-by: ares <ares@git.git>

---
+++ TestPages/src/controler/backup_EducationalBackgroundController.java
@@ -0,0 +1,98 @@
+package controler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import manager.EducationalBackgroundManager;
+import models.Courses;
+import models.EU;
+import models.GroupEU;
+
+/**
+ * 
+ * @author
+ *
+ */
+@ManagedBean(name = "EducationalBackgroundController")
+@SessionScoped
+public class backup_EducationalBackgroundController {
+
+	EducationalBackgroundManager EBM;
+	EUsController euM;
+	Courses theEducationalBackground;
+	List<EU> optionals;
+	
+	public backup_EducationalBackgroundController (){
+		EBM = new EducationalBackgroundManager();
+		euM = new EUsController();
+		init();
+	}
+
+	public String save() {
+		GroupEU tempo;
+		System.out.println(optionals.get(0).getName()); 
+//		for (EU eu : optionals) {
+//			tempo = new GroupEU();
+//			tempo.addEU(eu);
+//			theEducationalBackground.addOptions(tempo);
+//		}
+		EBM.save(theEducationalBackground);
+		init();
+		return "eus";
+	}
+	
+	public String remove(Courses educationalBackground) {
+		EBM.remove(educationalBackground);
+		return "eus";
+	}
+		
+	public List<EU> findAllEUs()
+	{
+		return euM.findAll();
+	}
+
+	public Courses getTheEducationalBackground() {
+		return theEducationalBackground;
+	}
+
+	public void setTheEducationalBackground(Courses theEducationalBackground) {
+		this.theEducationalBackground = theEducationalBackground;
+	}
+
+	public EducationalBackgroundManager getEBM() {
+		return EBM;
+	}
+
+	public void setEBM(EducationalBackgroundManager eBM) {
+		EBM = eBM;
+	}
+
+	public EUsController getEuM() {
+		return euM;
+	}
+
+	public void setEuM(EUsController euM) {
+		this.euM = euM;
+	}
+
+	public List<EU> getOptionals() {
+		return optionals;
+	}
+
+	public void setOptionals(List<EU> optionals) {
+		this.optionals = optionals;
+	}
+	
+	public void init()
+	{
+		theEducationalBackground = new Courses();
+		GroupEU gEU = new GroupEU();
+		gEU.setEus(new ArrayList<EU>());
+		theEducationalBackground.setObligatories(gEU);
+		optionals = new ArrayList<EU>();
+	}
+	
+}
